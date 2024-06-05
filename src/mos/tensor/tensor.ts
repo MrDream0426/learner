fadce11515783f5dcb4d0a84843e58a4bac3707f
cleverlove7 <ruodong.mo@procextech.com>
2024-06-05 08:19:33 +0800

    zeroTensor

--- src/mos/tensor/tensor.ts
+++ src/mos/tensor/tensor.ts
@@ -2,6 +2,12 @@ export type Tensor = number | Array<Tensor>
 
 export type Scalar = number
 
+export function zeroTensor(shape: Array<number>): Tensor {
+    if (!shape.length) {return 0}
+    if (shape.length === 1) {return Array(shape[0]).fill(0)}
+    else {return Array(shape[0]).fill(zeroTensor(shape.slice(1, shape.length)))}
+}
+
 
 export function isScalar(t: Tensor): t is number {
     return typeof(t) === "number"
@@ -9,6 +15,10 @@ export function isScalar(t: Tensor): t is number {
 
 export function isTensor1(t: Tensor): t is Array<number> {
     return rank(t) == 1
+}
+
+export function isSameShape(t1: Tensor, t2: Tensor): boolean {
+    return shape(t1) === shape(t2)
 }
 
 export function assertScalar(t: Tensor): asserts t is number {
@@ -20,6 +30,12 @@ export function assertScalar(t: Tensor): asserts t is number {
 export function assertTensor1(t: Tensor): asserts t is Array<number> {
     if (!isTensor1(t)) {
         throw new Error(`[assertTensor1] ${t}`)
+    }
+}
+
+export function assertSameShape(t1: Tensor, t2: Tensor): void {
+    if (!isSameShape(t1, t2)) {
+        throw new Error(`[assertSameShape] ${t1}, ${t2}`)
     }
 }
 
--- src/mos/tensor/test/tensor.test.ts
+++ src/mos/tensor/test/tensor.test.ts
@@ -1,6 +1,6 @@
 import test from "node:test";
 import assert from "node:assert";
-import { shape, rank } from "../tensor.js";
+import { shape, rank, zeroTensor } from "../tensor.js";
 
 
 test("mos -- shape", () =>{
@@ -13,4 +13,11 @@ test("mos -- rank", () => {
     assert.deepEqual(rank(1), 0)
     assert.deepEqual(rank([1]), 1)
     assert.deepEqual(rank([[1], [2], [3]]), 2)
+})
+
+test("mos -- zeroTensor", () => {
+    assert.deepEqual(zeroTensor([]), 0)
+    assert.deepEqual(zeroTensor([3]), [0, 0, 0])
+    assert.deepEqual(zeroTensor([2, 3]), [[0, 0, 0], [0, 0, 0]])
+    assert.deepEqual(zeroTensor([1, 2, 3]), [[[0, 0, 0],[0, 0, 0]]])
 })
